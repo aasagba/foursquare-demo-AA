115612009fe5ea3625657102dca1edfbc9a73ac2
Adrian Asagba <adrian@littleforest.co.uk>
2016-02-06 16:19:35 +0000

    basic form validation

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,4 +3,12 @@ var myApp = angular.module('myApp', ["ui.bootstrap"]);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("AppCtrl controller initialised");
 
+    /* search(location)
+     *
+     * This function will
+     */
+    $scope.search = function(location) {
+        console.log("Search term is: " + location);
+    }
+
 }]);
