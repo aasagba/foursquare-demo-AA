9b30fe8813cdc79721596a200b00f5aca9c8f3ce
Adrian Asagba <adrian@littleforest.co.uk>
2016-02-06 15:21:29 +0000

    setup angularjs

---
+++ public/controllers/controller.js
@@ -0,0 +1,6 @@
+var myApp = angular.module('myApp', ["ui.bootstrap"]);
+
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+    console.log("AppCtrl controller initialised");
+
+}]);
